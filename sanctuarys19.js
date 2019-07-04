cbc107e3fa5cd3ac8c1465f53bc7247e84124e15
Patrick Wilkie <plwilkie@gmail.com>
2019-07-03 18:26:05 -0700

    Added sanctuarys.js natively, updated html header to contain p5.js

---
+++ sanctuarys19.js
@@ -0,0 +1,40 @@
+function setup() {
+   createCanvas(screen.height, screen.width);
+}
+
+function draw() {
+   background(100);
+   //stroke(0);
+   let margin = 40; 
+   let ymargin = 10; // processing top bar height 
+   let x = width / 2 ; let y = height / 2 - ymargin;
+   //rotate(PI/2.0);
+   translate(x,y);
+   
+   // must draw inward from outside
+   // 24 levels including inner circle
+   //int level = 1;
+   let r = x;
+   
+   // level start on exterior (max radius), at 1 degree = 360 segments, moves inward.
+   for(let level = 1; level <= 360; level = level + 1){  
+      //print("ok");
+      if(360 % level == 0){
+         let i = true; // used for segment fill color
+         // n controls length of arc, as a function of the level increment starts at 1 degree, up to 360 degrees!
+         for(let n = -PI/2+mouseX; n <= TWO_PI - PI/2 + mouseX; n = n + level/360*TWO_PI){
+            //fill color in alternating black and white segments!
+            if(i==true){
+               fill(255);
+               i = false;
+            }
+            else{
+               fill(0);
+               i = true;
+            }
+            arc(0, 0, r, r, n, n + level/360*TWO_PI);
+         }
+         r = r - 21;
+      } 
+   }   
+}
