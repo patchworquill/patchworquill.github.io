df1ca2b8bc545c6c1f29ee0c51c2e4a1c2c93337
Patrick Wilkie <plwilkie@gmail.com>
2019-07-10 17:00:35 -0700

    Broke code, pulled from openprocessing again

--- sanctuarys19.js
+++ sanctuarys19.js
@@ -1,28 +1,23 @@
 function setup() {
-   let canvas = createCanvas(width, height*2/3);
+   let canvas = createCanvas(windowWidth, windowHeight-100);
    canvas.parent('sketch-holder');
    let dark = 0; let light = 255;
 }
 
 function draw() {
-   background(255);
+   background(100);
+   //stroke(0);
+   let margin = 40;
    let ymargin = 10; // processing top bar height
-
-   let x = width / 2 ; let y = height / 2 ;
+   let x = width / 2 ; let y = height / 2 - ymargin;
    //rotate(PI/2.0);
    translate(x,y);
 
    // must draw inward from outside
    // 24 levels including inner circle
-   if(x<y){
-      let r = x;
-   }
-   else{
-     let r = y;
-   }
-
+   //int level = 1;
+   let r = x;
    let barwidth = r / 24;
-
    // level start on exterior (max radius), at 1 degree = 360 segments, moves inward.
    for(let level = 1; level <= 360; level = level + 1){
       //print("ok");
@@ -41,22 +36,7 @@ function draw() {
             }
             arc(0, 0, r, r, n, n + level/360*TWO_PI);
          }
-         let m = 0;
-         if(mouseY > height){
-            m=1;
-            }
-
-         else if(mouseY < 0){
-         m=0;
-         }
-         else{
-         m = 23 * mouseY / height;
-         }
-         r = r - barwidth + m; //21
+         r = r - barwidth + 23*mouseY/screen.height;
       }
    }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight*2/3);
 }
